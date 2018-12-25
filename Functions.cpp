516dda276408e2995ba85caf50bc3fb79a923fb4
Anh Vu Nguyen <anhvu_nguyen@ucsb.edu>
2018-12-25 01:50:20 -0800

    Attempting to resimplify fractions after turning from improper into mixed

--- Functions.cpp
+++ Functions.cpp
@@ -9,7 +9,7 @@ using namespace std;
 
 //checks if fraction
 bool fracCheck(string temp){
-	if(temp.find("/")==1)
+	if(!(temp.find("/")==0))
 		return true;
 	return false;
 
@@ -18,6 +18,13 @@ bool fracCheck(string temp){
 int retrNum(string s){
 	int loc=s.find("/");
 	string temp= s.substr(0,loc);
+	//if string is a mixed fraction
+        if(!(s.find(" ")==0)){
+        int space=s.find(" ");
+	temp=s.substr(space,loc);
+
+}
+
 	const char *c= temp.c_str();
 	return atoi(c);
 }
@@ -30,6 +37,20 @@ int retrDenom(string s){
 
 }
 
+
+//simplifies an improper fraction
+string simplify(string a){
+int num=retrNum(a);
+int denom=retrDenom(a);
+int remainder=0;
+remainder=num/denom;
+num=num-(remainder*denom);
+return to_string(remainder)+" "+to_string(num)+"/"+to_string(denom);
+
+
+}
+
+
 string addition(string a,string b){
 //turns whole number integers into fractions
 	if(!fracCheck(a))
@@ -37,20 +58,25 @@ string addition(string a,string b){
 	if(!fracCheck(b))
                 b=b.insert(b.length(),"/1");
 //computation
-		int totalnum=(retrNum(a)*retrDenom(b))+(retrNum(b)*retrDenom(a));
-		int totaldenom=(retrDenom(a)*retrDenom(b));
-		
-		return to_string(totalnum)+"/"+to_string(totaldenom);
+	int totalnum=(retrNum(a)*retrDenom(b))+(retrNum(b)*retrDenom(a));
+	int totaldenom=(retrDenom(a)*retrDenom(b));
+	string final=  to_string(totalnum)+"/"+to_string(totaldenom);
+	if(retrNum(final)>retrDenom(final)){
+		final=simplify(final);
 	
+	return final;
 
 }
+}
+
 
 string multiplication(string a, string b){
+//turn whole number integers into fractions
 	if(!fracCheck(a))
                 a=a.insert(a.length(),"/1");
         if(!fracCheck(b))
                 b=b.insert(b.length(),"/1");
-
+//computation
 	int totalnum=retrNum(a)*retrNum(b);
 	int totaldenom=retrDenom(a)*retrDenom(b);
 	
